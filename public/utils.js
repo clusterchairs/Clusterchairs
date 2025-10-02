4099d654f4590c889f43f087ae587e50d0d66a1d
clusterchairs <clusterchairs@gmail.com>
2025-10-02 23:07:31 +0530

    Commit all files

--- public/utils.js
+++ public/utils.js
@@ -1,11 +1,23 @@
 // public/utils.js
 
 // Helper function to get a cookie's value
-function getCookie(name) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(';').shift();
-    return null;
-}
+// function getCookie(name) {
+//     const value = `; ${document.cookie}`;
+//     const parts = value.split(`; ${name}=`);
+//     if (parts.length === 2) return parts.pop().split(';').shift();
+//     return null;
+// }
 
 // You can add other useful helpers here later!
+
+function getCookie(name) {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+        let cookie = cookies[i].trim();
+        
+        if (cookie.startsWith(name + '=')) {
+            return decodeURIComponent(cookie.substring(name.length + 1));
+        }
+    }
+    return null;
+}
